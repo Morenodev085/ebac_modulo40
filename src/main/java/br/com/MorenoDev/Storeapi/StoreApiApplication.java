8fdd58c4aaa9a2e017dcb628cc515762887af3b6
Moreno Almeida <moreno.ferreira11@gmail.com>
2026-04-02 19:04:45 -0300

    ..

---
+++ src/main/java/br/com/MorenoDev/Storeapi/StoreApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.MorenoDev.Storeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApiApplication.class, args);
+	}
+
+}
